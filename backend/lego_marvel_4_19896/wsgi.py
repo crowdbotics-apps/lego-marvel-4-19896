763b4affab64c0ab4f8b33a519ffe597809be61e
Crowdbotics <team@crowdbotics.com>
2020-08-31 13:53:22 +0000

    Initial commit.

---
+++ backend/lego_marvel_4_19896/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lego_marvel_4_19896 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lego_marvel_4_19896.settings')
+
+application = get_wsgi_application()
---
+++ ios/lego_marvel_4_19896Tests/lego_marvel_4_19896Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface lego_marvel_4_19896 : XCTestCase
+
+@end
+
+@implementation lego_marvel_4_19896
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
